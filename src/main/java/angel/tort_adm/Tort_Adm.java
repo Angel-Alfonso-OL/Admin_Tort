59c5143fc7382aab1aeb54a2d6b22f012a20e31d
Angel Orozco <angelalfonsool20@gmail.com>
2023-12-14 20:20:48 -0700

    Start, without nothing add

---
+++ src/main/java/angel/tort_adm/Tort_Adm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package angel.tort_adm;
+
+/**
+ *
+ * @author angel
+ */
+public class Tort_Adm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
